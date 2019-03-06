654ba30c8d86297a35fe9c019aff2742a986522b
Shani <48276646+shanimartin28@users.noreply.github.com>
2019-03-06 14:54:24 +0100

    Add files via upload

---
+++ Para JSX.jsx
@@ -0,0 +1,15 @@
+/*
+Define a new constant JSX that renders a div which contains the following elements in order:
+An h1, a p, and an unordered list that contains three li items. You can include any text you want within each element.
+*/
+const JSX = 
+<div>
+  <h1>Paragraph One</h1>
+  <p>Paragraph Two</p>
+  <ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+  </ul>
+</div>
+;
