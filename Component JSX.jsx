c400b1f22da20a7573706a2805d974bbf1f8b959
Shani <48276646+shanimartin28@users.noreply.github.com>
2019-03-06 15:52:31 +0100

    Add files via upload

---
+++ Component JSX.jsx
@@ -0,0 +1,23 @@
+/*
+
+Components are the core of React. Everything in React is a component and here you will learn how to create one.
+
+There are two ways to create a React component. The first way is to use a JavaScript function. Defining a component in this way creates a stateless functional component. The concept of state in an application will be covered in later challenges. For now, think of a stateless component as one that can receive data and render it, but does not manage or track changes to that data. (We'll cover the second way to create a React component in the next challenge.)
+
+const MyComponent = function() {
+  // change code below this line
+ return (
+    <div> Hello </div>
+  );
+  // change code above this line
+}
+/*
+MyComponent should return JSX.
+MyComponent should return a div element.
+The div element should contain a string of text.
+*/
+
+
+*/
+
+
