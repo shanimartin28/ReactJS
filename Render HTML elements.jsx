e0393c664c899da4d4e962c625d8e830384f523b
Shani <48276646+shanimartin28@users.noreply.github.com>
2019-03-06 15:04:59 +0100

    Add files via upload

---
+++ Render HTML elements.jsx
@@ -0,0 +1,15 @@
+const JSX = (
+  <div>
+    <h1>Hello World</h1>
+    <p>Lets render this to the DOM</p>
+  </div>
+);
+// change code below this line
+ReactDOM.render(JSX, document.getElementById('challenge-node'));
+
+/*
+The constant JSX should return a div element.
+The div should contain an h1 tag as the first element.
+The div should contain a p tag as the second element.
+The provided JSX element should render to the DOM node with id challenge-node.
+*/
