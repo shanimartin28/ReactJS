60184d1b28c89eef31acd3c7ca4117c443c8da82
Shani <48276646+shanimartin28@users.noreply.github.com>
2019-03-06 15:33:47 +0100

    Add files via upload

---
+++ HTML class JSX.jsx
@@ -0,0 +1,11 @@
+const JSX = (
+  <div className = "myDiv">
+    <h1>Add a class to this div</h1> 
+  </div>
+);
+
+/*
+React: Define an HTML Class in JSX
+The constant JSX should return a div element.
+The div has a class of myDiv.
+*/
